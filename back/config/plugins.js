aad5a7853277f842de36ed69573549bc3bb0b19f
abraham-yusuf <1703020028@students.unis.ac.id>
2021-08-13 02:50:34 +0000

    add cloudinary

--- back/config/plugins.js
+++ back/config/plugins.js
@@ -27,6 +27,18 @@ module.exports = ({ env }) => {
           defaultReplyTo: 'kontak.abrahamyusuf@gmail.com',
         },
       },
+      upload: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
     };
   };
   
